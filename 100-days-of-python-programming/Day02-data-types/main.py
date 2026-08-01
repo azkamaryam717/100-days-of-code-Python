e9c6094de4957fc67b01c8d08758b7084732dbf5
azkamaryam717 <azkamaryam717@gmail.com>
2026-08-01 20:32:44 +0500

    Restructure Day 01 and add Day 02 data types

---
+++ 100-days-of-python-programming/Day02-data-types/main.py
@@ -0,0 +1,37 @@
+# int
+print(8) # positive integer
+print(-5) # negative integer
+print(10**100) # large integer
+
+# float
+print(8.9)
+print(1.8e307) # large finite float
+print(1e308) # very large finite float(near float limit)
+print(1e309) # infinity (overflow)
+print(1.8e308) # infinity (overflow)
+print(1.9e308) # infinity (overflow)
+
+# bool
+print(True)
+print(False)
+
+# str
+print("Hello World") # double string quotes
+print('Hello World') # single string quotes
+print("""Hello World""") # triple double quotes
+print('''Hello World''') # triple single quotes
+
+# complex
+print(1+4j)
+
+# list
+print([1,2,3,4])
+
+# tuple
+print((1,2,3,4))
+
+# set
+print({1,2,3})
+
+# dict
+print({"Name":"Azka","Age":20,"Gender":"Female"})
