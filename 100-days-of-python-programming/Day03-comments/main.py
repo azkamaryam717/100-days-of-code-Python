a8b111c8ab63d25b667fd125a6df0d831025c5b9
azkamaryam717 <azkamaryam717@gmail.com>
2026-08-02 18:58:24 +0500

    Complete Day 03 - comments

---
+++ 100-days-of-python-programming/Day03-comments/main.py
@@ -0,0 +1,7 @@
+# The Hash(#) symbol is used to write comments in Python. 
+# Comments improve code readability and help explain the code.
+
+# This is a single-line comment
+
+# For multi-line comments, start each line with a Hash(#) symbol 
+# Or select multiple lines and press Ctrl+/ (in VS Code) to comment them
