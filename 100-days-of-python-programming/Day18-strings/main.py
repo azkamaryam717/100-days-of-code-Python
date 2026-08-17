a4f63be09ecd0266ada794544cbc7dfae1126d51
azkamaryam717 <azkamaryam717@gmail.com>
2026-08-17 23:08:52 +0500

    Complete Day 18 - Strings

---
+++ 100-days-of-python-programming/Day18-strings/main.py
@@ -0,0 +1,21 @@
+# strings in Python
+# single quotes
+a = 'Hello'
+print(a)
+
+# double quotes
+a = "Hello"
+print(a)
+
+# triple quotes(multiline string)
+a = '''Hello,
+I am Azka'''
+print(a)
+
+# use of single and double quotes together
+# a= 'It's raining outside'(Syntax error)
+a = "It's raining outside" 
+print(a)
+
+a = str("Hello") # str does nothing useful here as Hello is already a string
+print(a)
