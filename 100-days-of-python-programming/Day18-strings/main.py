cb0ace9a397fa78a8d0c3c58b36a11aecd9cb293
azkamaryam717 <azkamaryam717@gmail.com>
2026-08-18 21:51:13 +0500

    Modifies Day 18 -  String Slicing

--- 100-days-of-python-programming/Day18-strings/main.py
+++ 100-days-of-python-programming/Day18-strings/main.py
@@ -19,3 +19,22 @@ print(a)
 
 a = str("Hello") # str does nothing useful here as Hello is already a string
 print(a)
+
+# Accessing Substrings from a String
+# Indexing
+a = "Hello"
+print(a[0]) # Positive Indexing
+print(a[-1]) # Negative Indexing
+
+# Slicing
+a = "Hello World"
+print(a[0:6]) # indices 0-5
+print(a[3:]) # Starts from index 3 and goes to the end
+print(a[:4]) # Assumes that you started slicing from 0-3
+print(a[:]) # Assumes you want to print full string
+print(a[2:6:2]) # 2-6 but skips one element after printing one
+print(a[0:10:3]) # 0-10 but skips two elements after printing one
+print(a[0:6:-1])  # Empty because the step is backwards but start < stop
+print(a[-5:-1:2]) # -5 to -1 with skipping sfter printing one
+print(a[::-1]) # prints starts to end but -1 makes it backwards
+print(a[-1:-6:-1]) # -1 to -6 and prints backwards
