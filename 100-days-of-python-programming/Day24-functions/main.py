3ab87a9efff631e5ff525167a25ec8364f1bc8cf
azkamaryam717 <azkamaryam717@gmail.com>
2026-08-30 22:01:37 +0500

    Day 24 - Parameters and Arguments

--- 100-days-of-python-programming/Day24-functions/main.py
+++ 100-days-of-python-programming/Day24-functions/main.py
@@ -18,3 +18,41 @@ for i in range(1,11):
 print(is_even.__doc__)
 print(print.__doc__)
 print(type.__doc__)
+
+# Parameters vs Arguments
+def power(a, b):
+    return a**b
+print(power(2,3))
+# power(3) # invalid requires 2 arguments
+# power() # invalid requires 2 arguments
+# Default Argument: Function arguments with default values.
+def power(a = 1, b = 1):
+    return(a**b)
+print(power(2, 3))
+print(power(2))
+print(power())
+# Positional Arguments: Values assigned by call order.
+print(power(3, 2))
+# Keyword Argument: Values assigned to args by name at call time.
+# Keyword args will Overrides Positional args.
+print(power(b = 2, a = 3))
+# Arbitrary Argument: Accepts any number of args.
+# Useful when the number of arguments is unknown.
+def flexi(*number):
+    product = 1
+    for i in number:
+        product *= i
+    print(product)
+print(flexi(1))
+print(flexi(1, 2))
+print(flexi(1, 2, 3))
+print(flexi(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+def flexi(*number): # Flexible inputs ---> tuple
+    product = 1
+    print(number)
+    print(type)
+    for i in number:
+        product *= i
+    print(product)
+print(flexi(1, 2, 3, 4, 5))
