59611b267b62b184f7cb6694e35dafc8bf2e3e11
azkamaryam717 <azkamaryam717@gmail.com>
2026-09-01 21:33:20 +0500

    Day 24 functions - Global and Local Variables

--- 100-days-of-python-programming/Day24-functions/main.py
+++ 100-days-of-python-programming/Day24-functions/main.py
@@ -76,3 +76,18 @@ def display(**Azka):
     print(key, '->', value)
 
 print(display(srilanka='colombo', nepal='kathmandu', pakistan='islamabad'))
+
+# Global Var and Local Var
+# Functions as Arguments
+def func_a():
+    print("inside func_a: ")
+    # No return value ---> `None`
+def func_b(y):
+    print("inside func_b: ")
+    return y
+def func_c(z):
+    print("inside func_c: ")
+    return z()
+print(func_a())
+print(5 + func_b(2))
+print(func_c(func_a))
